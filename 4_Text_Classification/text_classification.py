e7ea7b579a8e13b0605b155563f7a0d99515bace
Julian Wise - WLAPTOP <Julianadamwise@gmail.com>
2016-07-31 01:23:51 +1000

    a generic method of gaining a boolean dictionary of word occurence from training data

--- 4_Text_Classification/text_classification.py
+++ 4_Text_Classification/text_classification.py
@@ -26,6 +26,28 @@ for w in movie_reviews.words():
 
 # Convert list to frequency distribution and print most common words
 all_words = nltk.FreqDist(all_words)
-print all_words.most_common(15)
+#print all_words.most_common(15)
+
+# Train on the top 3000 words
+word_features = list(all_words.keys())[:3000]
+# training_data = "this sentence string format yellow orange purple green" 
+# word_features  = training_data.split()
+
+# A set removes doubled elements, only single representation
+def find_features(document):
+	words = set(document)
+	features = {}
+	for w in word_features:
+		# (w in words) creates boolean value for each word, if word in document it will return true, else it will return false
+		features[w] = (w in words) 
+	return features
+
+# sentence = "this is a sentence of words in a string format alfalfa"
+# feat = find_features(sentence)
+# print feat
+
+print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
 
 print 'kitty'
