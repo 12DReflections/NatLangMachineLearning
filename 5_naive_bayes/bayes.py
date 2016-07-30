24e9f40e369497f45e32519ffe686646cbd44f51
Julian Wise - WLAPTOP <Julianadamwise@gmail.com>
2016-07-31 01:40:08 +1000

    Perform a naive bayes to see whether words lead to a positive or negative category of articles

---
+++ 5_naive_bayes/bayes.py
@@ -0,0 +1,64 @@
+import nltk
+import random
+from nltk.corpus import movie_reviews
+
+# Text Classification with nltk library
+# Works if you have a body of a words and a boolean classification ie (positive, negative) as a tuple
+
+
+documents = [(list(movie_reviews.words(fileid)), category) 
+				for category in movie_reviews.categories() 
+				for fileid in movie_reviews.fileids(category)]
+
+# # Same as 
+# documents = []
+# for category in movie_reviews.categories():
+# 	for fileid in movie_reviews.fileids(category):
+# 		documents.append(list(movie_reviews.words(fileid)), category)
+
+# Train data
+random.shuffle(documents)
+
+# Get the full list of words
+all_words = []
+for w in movie_reviews.words():
+	all_words.append(w.lower())
+
+# Convert list to frequency distribution and print most common words
+all_words = nltk.FreqDist(all_words)
+#print all_words.most_common(15)
+
+# Train on the top 3000 words
+word_features = list(all_words.keys())[:3000]
+# training_data = "this sentence string format yellow orange purple green" 
+# word_features  = training_data.split()
+
+# A set removes doubled elements, only single representation
+def find_features(document):
+	words = set(document)
+	features = {}
+	for w in word_features:
+		# (w in words) creates boolean value for each word, if word in document it will return true, else it will return false
+		features[w] = (w in words) 
+	return features
+
+# sentence = "this is a sentence of words in a string format alfalfa"
+# feat = find_features(sentence)
+# print feat
+
+#print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
+
+# The existence of words in a document, from our training words, and whether they lead to a positive/negative category
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
+#print featuresets[1]
+
+training_set = featuresets[:1900]
+testing_set = featuresets[1900:]
+
+# Naive bayes
+# post_occurence = prior_occurrence * liklihood / evidence 
+# Show whether a word is more likely to be in a positive or negative review
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+print("Naive Bayes Algo accuracy percent: ", (nltk.classify.accuracy(classifier, training_set)) * 100)
+classifier.show_most_informative_features(15)
+print 'kitty'
